dbd2831c70c2cda93fa0dffa63a9ade0e12b4783
Junwei Yu <junweiyu@MacBook-Pro-som-tillhor-Junwei.local>
2024-04-05 18:43:49 +0200

    turn

--- src/main/java/Junbot.java
+++ src/main/java/Junbot.java
@@ -23,31 +23,14 @@ public class Junbot extends Robot
 		setRadarColor(new Color(0, 100, 100));
 		setBulletColor(new Color(255, 255, 100));
 		setScanColor(new Color(255, 200, 200));
+		double heading = getHeading();
+		turnLeft(heading);
+		double gunheading = getGunHeading();
+		turnGunLeft(gunheading);
 		// Robot main loop
 		while(true) {
 			// Replace the next 4 lines with any behavior you would like
-			setAhead(40000);
-			movingForward = true;
-			// Tell the game we will want to turn right 90
-			setTurnRight(90);
-			// At this point, we have indicated to the game that *when we do something*,
-			// we will want to move ahead and turn right.  That's what "set" means.
-			// It is important to realize we have not done anything yet!
-			// In order to actually move, we'll want to call a method that
-			// takes real time, such as waitFor.
-			// waitFor actually starts the action -- we start moving and turning.
-			// It will not return until we have finished turning.
-			waitFor(new TurnCompleteCondition(this));
-			// Note:  We are still moving ahead now, but the turn is complete.
-			// Now we'll turn the other way...
-			setTurnLeft(180);
-			// ... and wait for the turn to finish ...
-			waitFor(new TurnCompleteCondition(this));
-			// ... then the other way ...
-			setTurnRight(180);
-			// .. and wait for that turn to finish.
-			waitFor(new TurnCompleteCondition(this));
-			// then back to the top to do it all again
+			ahead(100);
 		}
 	}
 
@@ -64,19 +47,16 @@ public class Junbot extends Robot
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
 		// Replace the next line with any behavior you would like
-		if (e.isMyFault()) {
-			reverseDirection();
-		}
+		turnRight(90);
+		turnRadarRight(90);
+		turnGunRight(90);
 	}
 
-	/**
-	 * onHitRobot:  Back up!
-	 */
-	public void onHitRobot(HitRobotEvent e) {
-		// If we're moving the other robot, reverse!
-		if (e.isMyFault()) {
-			reverseDirection();
-		}
+	public void onHitRobot(HitRobotEvent event){
+		// Replace the next line with any behavior you would like
+		turnRight(90);
+		turnRadarRight(90);
+		turnGunRight(90);
 	}
 	
 	/**
@@ -84,16 +64,8 @@ public class Junbot extends Robot
 	 */
 	public void onHitWall(HitWallEvent e) {
 		// Replace the next line with any behavior you would like
-		reverseDirection();
-	}
-
-	public void reverseDirection() {
-		if (movingForward) {
-			setBack(40000);
-			movingForward = false;
-		} else {
-			setAhead(40000);
-			movingForward = true;
-		}
+		turnRight(90);
+		turnRadarRight(90);
+		turnGunRight(90);
 	}
 }
